796473064e11764861d198e4ffe5cbcfdf589efd
Mickey <mickeyth59@gmail.com>
2024-07-02 23:19:01 +0530

    build

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-// src/redux/reducers/index.js
-import { combineReducers } from 'redux';
-import movieReducer from './movieReducer'; // We'll define this next
-
-const rootReducer = combineReducers({
-  movies: movieReducer, // You can add more reducers here
-});
-
-export default rootReducer;
